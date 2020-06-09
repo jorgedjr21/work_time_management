34a79542e2cf34a7d9aa658d4cea33a1b75841b3
Jorge Junior <jorgedjr21@gmail.com>
2020-06-09 20:18:20 -0300

    register and login pages rework

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+
+  validates :name, presence: true, allow_blank: false
+  validates :email, uniqueness: { case_sensitive: false }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
